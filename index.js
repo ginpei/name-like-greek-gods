3e6116211018beeb3e3b4453c5ffbe53bb008300
ginpei <ginpei@gmail.com>
2015-10-19 00:36:28 -0700

    refactor token names

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var url = 'https://ja.wikipedia.org/wiki/%E3%82%AE%E3%83%AA%E3%82%B7%E3%82%A2%E7%A5%9E%E8%A9%B1';
-var prefix = 'サカナ';
+var baseName = 'サカナ';
 
-var delimiter = prefix[prefix.length-1];
+var delimiter = baseName[baseName.length-1];
 
 
 var webpage;
@@ -27,10 +27,10 @@ page.open(url, function(status) {
 	var names = unique(kataWords);
 	var filteredNames = filterByNa(names);
 	var namesFilteredNicely = filterNicely(filteredNames);
-	var sakanaNames = injectSakana(namesFilteredNicely);
+	var resultNames = injectBaseName(namesFilteredNicely);
 
-	for (var i=0, l=sakanaNames.length; i<l; i++) {
-		console.log('-', sakanaNames[i]);
+	for (var i=0, l=resultNames.length; i<l; i++) {
+		console.log('-', resultNames[i]);
 	}
 
 	console.log('done.');
@@ -131,12 +131,12 @@ function filterNicely(names) {
 	return filtered;
 }
 
-function injectSakana(names) {
+function injectBaseName(names) {
 	var injecteds = [];
 	for (var i=0, l=names.length; i<l; i++) {
 		var name = names[i];
 		var index = name.indexOf(delimiter);
-		var injected = prefix + name.slice(index+1);
+		var injected = baseName + name.slice(index+1);
 		injecteds.push(injected);
 	}
 	return injecteds;
