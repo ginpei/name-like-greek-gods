93f589d428a404a7421b943b85f78906178378f2
ginpei <ginpei@gmail.com>
2015-10-19 00:26:34 -0700

    indent

--- index.js
+++ index.js
@@ -15,80 +15,80 @@ page.onConsoleMessage = function(msg) {
 	console.log(msg);
 };
 page.open(url, function(status) {
-		var prefix = 'サカナ';
+	var prefix = 'サカナ';
 
-		var delimiter = prefix[prefix.length-1];
+	var delimiter = prefix[prefix.length-1];
 
-		function getKataWords(text) {
-			var rxKata = /[ア-ンヴー]+/g;
-			var names = text.match(rxKata) || [];
-			return names;
+	function getKataWords(text) {
+		var rxKata = /[ア-ンヴー]+/g;
+		var names = text.match(rxKata) || [];
+		return names;
+	}
+
+	function unique(names) {
+		var filtered = [];
+		var hash = {};
+
+		for (var i=0, l=names.length; i<l; i++) {
+			var name = names[i];
+			if (!(name in hash)) {
+				hash[name] = true;
+				filtered.push(name);
+			}
 		}
 
-		function unique(names) {
-			var filtered = [];
-			var hash = {};
+		return filtered;
+	}
 
-			for (var i=0, l=names.length; i<l; i++) {
-				var name = names[i];
-				if (!(name in hash)) {
-					hash[name] = true;
-					filtered.push(name);
+	function filterByNa(names) {
+		var filtered = [];
+		for (var i=0, l=names.length; i<l; i++) {
+			var name = names[i];
+			if (name.indexOf(delimiter) >= 0) {
+				filtered.push(name);
+			}
+		}
+		return filtered;
+	}
+
+	function filterNicely(names) {
+		var deniedSufixes = [delimiter, delimiter+'イ', delimiter+'ー'];
+		var filtered = [];
+		for (var i=0, l=names.length; i<l; i++) {
+			var name = names[i];
+			var ok = true;
+			for (var j=0, m=deniedSufixes.length; j<m; j++) {
+				var sufix = deniedSufixes[j];
+				if (name.indexOf(sufix) === name.length-sufix.length) {
+					ok = false;
+					break;
 				}
 			}
-
-			return filtered;
-		}
-
-		function filterByNa(names) {
-			var filtered = [];
-			for (var i=0, l=names.length; i<l; i++) {
-				var name = names[i];
-				if (name.indexOf(delimiter) >= 0) {
-					filtered.push(name);
-				}
-			}
-			return filtered;
-		}
-
-		function filterNicely(names) {
-			var deniedSufixes = [delimiter, delimiter+'イ', delimiter+'ー'];
-			var filtered = [];
-			for (var i=0, l=names.length; i<l; i++) {
-				var name = names[i];
-				var ok = true;
-				for (var j=0, m=deniedSufixes.length; j<m; j++) {
-					var sufix = deniedSufixes[j];
-					if (name.indexOf(sufix) === name.length-sufix.length) {
-						ok = false;
-						break;
-					}
-				}
-				if (ok) {
-					filtered.push(name);
-				}
-			}
-			return filtered;
-		}
-
-		function showNames(names) {
-			for (var i=0, l=names.length; i<l; i++) {
-				console.log('-', names[i]);
+			if (ok) {
+				filtered.push(name);
 			}
 		}
+		return filtered;
+	}
 
-		function injectSakana(names) {
-			var injecteds = [];
-			for (var i=0, l=names.length; i<l; i++) {
-				var name = names[i];
-				var index = name.indexOf(delimiter);
-				var injected = prefix + name.slice(index+1);
-				injecteds.push(injected);
-			}
-			return injecteds;
+	function showNames(names) {
+		for (var i=0, l=names.length; i<l; i++) {
+			console.log('-', names[i]);
 		}
+	}
 
-		console.log('OK');
+	function injectSakana(names) {
+		var injecteds = [];
+		for (var i=0, l=names.length; i<l; i++) {
+			var name = names[i];
+			var index = name.indexOf(delimiter);
+			var injected = prefix + name.slice(index+1);
+			injecteds.push(injected);
+		}
+		return injecteds;
+	}
+
+	console.log('OK');
 	page.evaluate(function() {
 		console.log('#', document.title);
 	});
@@ -122,14 +122,14 @@ page.open(url, function(status) {
 		return texts;
 	});
 
-		var kataWords = getKataWords(texts.join(' '));
-		var names = unique(kataWords);
-		var filteredNames = filterByNa(names);
-		var namesFilteredNicely = filterNicely(filteredNames);
-		var sakanaNames = injectSakana(namesFilteredNicely);
-		showNames(sakanaNames);
+	var kataWords = getKataWords(texts.join(' '));
+	var names = unique(kataWords);
+	var filteredNames = filterByNa(names);
+	var namesFilteredNicely = filterNicely(filteredNames);
+	var sakanaNames = injectSakana(namesFilteredNicely);
+	showNames(sakanaNames);
 
-		console.log('done.');
+	console.log('done.');
 });
 
 console.log('Fetching...');
