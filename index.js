490152ed79140b3520042d37d48aa1cb9537a999
ginpei <ginpei@gmail.com>
2015-10-19 00:29:33 -0700

    move code positions

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 var url = 'https://ja.wikipedia.org/wiki/%E3%82%AE%E3%83%AA%E3%82%B7%E3%82%A2%E7%A5%9E%E8%A9%B1';
+var prefix = 'サカナ';
+
+var delimiter = prefix[prefix.length-1];
+
 
 var webpage;
 try {
@@ -15,79 +19,6 @@ page.onConsoleMessage = function(msg) {
 	console.log(msg);
 };
 page.open(url, function(status) {
-	var prefix = 'サカナ';
-
-	var delimiter = prefix[prefix.length-1];
-
-	function getKataWords(text) {
-		var rxKata = /[ア-ンヴー]+/g;
-		var names = text.match(rxKata) || [];
-		return names;
-	}
-
-	function unique(names) {
-		var filtered = [];
-		var hash = {};
-
-		for (var i=0, l=names.length; i<l; i++) {
-			var name = names[i];
-			if (!(name in hash)) {
-				hash[name] = true;
-				filtered.push(name);
-			}
-		}
-
-		return filtered;
-	}
-
-	function filterByNa(names) {
-		var filtered = [];
-		for (var i=0, l=names.length; i<l; i++) {
-			var name = names[i];
-			if (name.indexOf(delimiter) >= 0) {
-				filtered.push(name);
-			}
-		}
-		return filtered;
-	}
-
-	function filterNicely(names) {
-		var deniedSufixes = [delimiter, delimiter+'イ', delimiter+'ー'];
-		var filtered = [];
-		for (var i=0, l=names.length; i<l; i++) {
-			var name = names[i];
-			var ok = true;
-			for (var j=0, m=deniedSufixes.length; j<m; j++) {
-				var sufix = deniedSufixes[j];
-				if (name.indexOf(sufix) === name.length-sufix.length) {
-					ok = false;
-					break;
-				}
-			}
-			if (ok) {
-				filtered.push(name);
-			}
-		}
-		return filtered;
-	}
-
-	function showNames(names) {
-		for (var i=0, l=names.length; i<l; i++) {
-			console.log('-', names[i]);
-		}
-	}
-
-	function injectSakana(names) {
-		var injecteds = [];
-		for (var i=0, l=names.length; i<l; i++) {
-			var name = names[i];
-			var index = name.indexOf(delimiter);
-			var injected = prefix + name.slice(index+1);
-			injecteds.push(injected);
-		}
-		return injecteds;
-	}
-
 	console.log('OK');
 	page.evaluate(function() {
 		console.log('#', document.title);
@@ -134,3 +65,75 @@ page.open(url, function(status) {
 
 console.log('Fetching...');
 console.log(url);
+
+// --------------------------------
+
+function getKataWords(text) {
+	var rxKata = /[ア-ンヴー]+/g;
+	var names = text.match(rxKata) || [];
+	return names;
+}
+
+function unique(names) {
+	var filtered = [];
+	var hash = {};
+
+	for (var i=0, l=names.length; i<l; i++) {
+		var name = names[i];
+		if (!(name in hash)) {
+			hash[name] = true;
+			filtered.push(name);
+		}
+	}
+
+	return filtered;
+}
+
+function filterByNa(names) {
+	var filtered = [];
+	for (var i=0, l=names.length; i<l; i++) {
+		var name = names[i];
+		if (name.indexOf(delimiter) >= 0) {
+			filtered.push(name);
+		}
+	}
+	return filtered;
+}
+
+function filterNicely(names) {
+	var deniedSufixes = [delimiter, delimiter+'イ', delimiter+'ー'];
+	var filtered = [];
+	for (var i=0, l=names.length; i<l; i++) {
+		var name = names[i];
+		var ok = true;
+		for (var j=0, m=deniedSufixes.length; j<m; j++) {
+			var sufix = deniedSufixes[j];
+			if (name.indexOf(sufix) === name.length-sufix.length) {
+				ok = false;
+				break;
+			}
+		}
+		if (ok) {
+			filtered.push(name);
+		}
+	}
+	return filtered;
+}
+
+function showNames(names) {
+	for (var i=0, l=names.length; i<l; i++) {
+		console.log('-', names[i]);
+	}
+}
+
+function injectSakana(names) {
+	var injecteds = [];
+	for (var i=0, l=names.length; i<l; i++) {
+		var name = names[i];
+		var index = name.indexOf(delimiter);
+		var injected = prefix + name.slice(index+1);
+		injecteds.push(injected);
+	}
+	return injecteds;
+}
+
