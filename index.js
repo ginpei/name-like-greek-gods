67c41cf658a520f71f9c92b59ff5225f4dd1bb9d
ginpei <ginpei@gmail.com>
2015-10-20 10:25:12 -0700

    order

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var defaultUrl = 'https://ja.wikipedia.org/wiki/%E3%82%AE%E3%83%AA%E3%82%B7%E3%82%A2%E7%A5%9E%E8%A9%B1';
 var defaultBaseName = 'サカナ';
+var defaultUrl = 'https://ja.wikipedia.org/wiki/%E3%82%AE%E3%83%AA%E3%82%B7%E3%82%A2%E7%A5%9E%E8%A9%B1';
 
 var baseName = phantom.args[0] || defaultBaseName;
 var url = phantom.args[1] || defaultUrl;
